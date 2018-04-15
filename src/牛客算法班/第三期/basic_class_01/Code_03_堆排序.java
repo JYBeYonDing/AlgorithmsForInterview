d7408ec032c4ceb14f9a93c58d40d7e4bf932825
James Young <1370319648@qq.com>
2018-04-15 18:20:31 +0800

    牛客

--- src/牛客算法班/第三期/basic_class_01/Code_03_堆排序.java
+++ src/牛客算法班/第三期/basic_class_01/Code_03_堆排序.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 堆 其实是一个 完全二叉树
+ * 用一个 大根堆 和一个 小根堆 可以求数据流的中位数 牛客算法班.第三期.basic_class_04.Code_01_MadianQuick
  */
 public class Code_03_堆排序 {
 
@@ -11,11 +12,11 @@ public class Code_03_堆排序 {
 		if (arr == null || arr.length < 2) {
 			return;
 		}
-		//建大根堆
+		//建大根堆 log(1)+log(2)+log(3)+...+log(N) -> N ???怀疑
 		for (int i = 0; i < arr.length; i++) {
-			heapInsert(arr, i);
+			heapInsert(arr, i);//[0,i]变成大根堆
 		}
-		//每次取堆顶元素，Java数据结构和算法.排序
+		//每次取堆顶元素
 		int size = arr.length;
 		swap(arr, 0, --size);
 		while (size > 0) {
@@ -24,18 +25,29 @@ public class Code_03_堆排序 {
 		}
 	}
 
+	/**
+	 * 插入arr[index]，建立大根堆
+	 */
 	public static void heapInsert(int[] arr, int index) {
 		// -1/2 = 0/2 = 1/2 = 0 所以不会越界 2/2 = 3/2 = 1
+		// (0-1)/2 = 0，所以0的父节点是自己
 		// 由于-1 的补码为 全1 所以这里不能用 >>1 代替 /2, 否则会越界
-		while (arr[index] > arr[(index - 1) / 2]) {
+		while (arr[index] > arr[(index - 1) / 2]) {// 该节点大于父节点 则交换
 			swap(arr, index, (index - 1) / 2);
 			index = (index - 1) / 2;
 		}
 	}
 
+	/**
+	 * 调整大根堆
+	 * 复杂度只有 logN
+	 * @param arr
+	 * @param index
+	 * @param size 堆的大小
+	 */
 	public static void heapify(int[] arr, int index, int size) {
 		int left = index * 2 + 1;
-		while (left < size) {
+		while (left < size) {// 如果有左孩子
 			// 左右子节点中取一个最大值，如果只有左，则取左节点
 			int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;
 			// 左右子节点中的最大值和当前节点比较，取最大值
