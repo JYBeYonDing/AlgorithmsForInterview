6f1716a10d66898f39d1b02c21c2545067e62125
yangjie <jamesyoung94@163.com>
2022-09-09 12:47:47 +0800

    add:Q1注释

--- src/LeetCode/Q1两数之和.java
+++ src/LeetCode/Q1两数之和.java
@@ -8,16 +8,24 @@ import java.util.Map;
  */
 public class Q1两数之和 {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
         return null;
     }
 
+    /**
+     * 采用map，以时间换空间的方式进行，可能存在不同的索引存的是相同的数，但是题目中只需要返回一个即可
+     *
+     * @param nums
+     * @param target
+     *
+     * @return
+     */
     public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -26,18 +34,26 @@ public class Q1两数之和 {
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] { i, map.get(complement) };
+                return new int[]{i, map.get(complement)};
             }
         }
         throw new IllegalArgumentException("No two sum solution");
     }
 
+    /**
+     * 在方法2上进一步优化，可能不需要遍历完就可以取出一个结果
+     *
+     * @param nums
+     * @param target
+     *
+     * @return
+     */
     public int[] twoSum3(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+                return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
